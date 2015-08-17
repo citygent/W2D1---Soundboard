daa2b2175ff46162876e9b2501a7ee1bf11da91f
James White <cyberyt@gmail.com>
2015-08-17 21:24:46 +0100

    building listeners, playsound function added

--- js/music.js
+++ js/music.js
@@ -1,8 +1,45 @@
+console.log('Hi Jimmy');
+soundManager.setup({
   url: '/swf/',
-  flashVersion: 9, // optional: shiny features (default = 8)
-  // optional: ignore Flash where possible, use 100% HTML5 mode
-  // preferFlash: false,
+  flashVersion: 9,
   onready: function() {
-    // Ready to use; soundManager.createSound() etc. can now be called.
+    setupListeners();
+  },
+  ontimeout: function() {
+    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
   }
 });
+
+function setupListeners() {
+  // playButton = document.getElementById('playbutton');
+  // playButton.addEventListener('click', sound1);
+
+  hope = document.getElementById('hope');
+  hope.addEventListener('click', function (){playSound('hope');});
+  king = document.getElementById('king');
+  king.addEventListener('click', function (){playSound('king');});
+  shimano = document.getElementById('shimano');
+  shimano.addEventListener('click', function (){playSound('shimano');});
+  campagnolo = document.getElementById('campagnolo');
+  campagnolo.addEventListener('click', function (){playSound('campagnolo');});
+  white = document.getElementById('white');
+  white.addEventListener('click', function (){playSound('white');});
+  power = document.getElementById('power');
+  power.addEventListener('click', function (){playSound('power');});
+}
+
+function playSound(sound){
+  var mySound = soundManager.createSound({
+    id: mySound,
+    url: './sounds/'+sound+'.mp3'
+  });
+  mySound.play();
+}
+
+// function sound1(){
+//     var mySound = soundManager.createSound({
+//       id: 'drumloop',
+//       url: './sounds/drumloop.mp3'
+//     });
+//     mySound.play();
+//   }
