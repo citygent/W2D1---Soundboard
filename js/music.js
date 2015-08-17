aa9e1c3be578d851d6db310e407aa4d7a710a5a1
James White <cyberyt@gmail.com>
2015-08-17 19:57:34 +0100

    built css & html, added starter files

---
+++ js/music.js
@@ -0,0 +1,8 @@
+  url: '/swf/',
+  flashVersion: 9, // optional: shiny features (default = 8)
+  // optional: ignore Flash where possible, use 100% HTML5 mode
+  // preferFlash: false,
+  onready: function() {
+    // Ready to use; soundManager.createSound() etc. can now be called.
+  }
+});
